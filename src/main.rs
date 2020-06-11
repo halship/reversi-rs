f4de99b81f0c95e71b06b98b48c41b5eec461e97
halship <gship@hotmail.co.jp>
2020-06-11 20:55:20 +0900

    Add Player

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,46 @@
 mod board;
+mod player;
 mod resources;
 
-use board::Board;
+use board::{Board, Stone};
 use ggez::conf::{WindowMode, WindowSetup};
-use ggez::event::{self, EventHandler};
+use ggez::event::{self, EventHandler, MouseButton};
 use ggez::{graphics, timer, Context, ContextBuilder, GameResult};
+use player::Player;
 use resources::Resources;
 
 struct Game {
     res: Resources,
     board: Board,
+    player: Vec<Player>,
+    tern: usize,
 }
 
 impl Game {
     fn new(ctx: &mut Context) -> GameResult<Self> {
         let res = Resources::new(ctx)?;
         let board = Board::new();
+        let player = vec![
+            Player::new_human(Stone::Black),
+            Player::new_human(Stone::White),
+        ];
 
-        Ok(Self { res, board })
+        Ok(Self {
+            res,
+            board,
+            player,
+            tern: 0,
+        })
     }
 }
 
 impl EventHandler for Game {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        while timer::check_update_time(ctx, 60) {}
+        while timer::check_update_time(ctx, 60) {
+            if self.player[self.tern].put_stone(&mut self.board) {
+                self.tern = (self.tern + 1) % 2;
+            }
+        }
         Ok(())
     }
 
@@ -31,6 +48,10 @@ impl EventHandler for Game {
         graphics::clear(ctx, graphics::BLACK);
         self.board.draw(ctx, &mut self.res)?;
         graphics::present(ctx)
+    }
+
+    fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
+        self.player[self.tern].mouse_button_down(&button, x, y);
     }
 }
 
---
+++ src/player.rs
@@ -0,0 +1,68 @@
+use crate::board::{Board, Stone};
+use ggez::event::MouseButton;
+
+pub enum Player {
+    Human {
+        stone: Stone,
+        idx: Option<(usize, usize)>,
+    },
+}
+
+impl Player {
+    pub fn new_human(stone: Stone) -> Self {
+        Self::Human { stone, idx: None }
+    }
+
+    pub fn stone(&self) -> Stone {
+        match self {
+            Player::Human { stone, .. } => *stone,
+        }
+    }
+
+    pub fn put_stone(&self, board: &mut Board) -> bool {
+        match self {
+            Player::Human {
+                stone,
+                idx: Some((i, j)),
+            } => {
+                if board.reversible_n(*stone, *i, *j) != 0 {
+                    board.set(*stone, *i, *j);
+                    true
+                } else {
+                    false
+                }
+            }
+            _ => false,
+        }
+    }
+
+    pub fn mouse_button_down(&mut self, button: &MouseButton, x: f32, y: f32) {
+        match self {
+            Player::Human { idx, .. } => {
+                if *button == MouseButton::Left {
+                    *idx = Some(((y / 50.0) as usize + 1, (x / 50.0) as usize + 1));
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn human_on_mouse_button_down() {
+        let mut player = Player::new_human(Stone::Black);
+        player.mouse_button_down(&MouseButton::Left, 120.0, 230.0);
+        match player {
+            Player::Human {
+                idx: Some((i, j)), ..
+            } => {
+                assert_eq!(4, i);
+                assert_eq!(2, j);
+            }
+            _ => (),
+        }
+    }
+}
